d683a9e8fc61955fbc2a29abc2a7891d3243c27d
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-06-06 14:21:10 +0300

    add

--- others/t_1111617/app/modal-page.ts
+++ others/t_1111617/app/modal-page.ts
@@ -6,6 +6,8 @@ import { Color } from "color";
 import { ListView } from "ui/list-view";
 import { RadListView } from "nativescript-telerik-ui-pro/listview";
 
+import { isIOS } from "platform";
+
 import * as utilsModule from "utils/utils";
 
 var listView;
@@ -17,7 +19,6 @@ export function pageStoriesPagesLoaded(args) {
     page = args.object;
     page.bindingContext = new ViewModel();
 }
-
 
 export function onRadListLoaded(args) {
     let radList = <RadListView>args.object;
@@ -35,9 +36,34 @@ export function onItemLoading(args) {
 };
 
 function setIosListItemTransparentBackground(args) {
-    if (isRad) { //RadListView?
-        console.log("isRad RadListView");
-        var iosColor = new Color(20, 255, 0, 0).ios;
-        args.ios.backgroundView.backgroundColor = iosColor;
+    console.log("instance of RadListView: " + (args.view.parent instanceof RadListView));
+
+    if (isIOS) {
+        if (args.view.parent instanceof RadListView) {
+            //transparent, so that list item has same color as page-background
+            var iosColor = new Color(0, 0, 0, 0).ios;
+            args.ios.backgroundView.backgroundColor = iosColor;
+        }
     }
 }
+
+/*  Here are the steps to apply the transparent mode for your modal page.
+- run npm install
+- go to node_modules/tns-core-modules/ui/page/page.ios.js
+- find the method called _showNativeModalView
+- change this code:
+
+// if (fullscreen) {
+//     this._ios.modalPresentationStyle = 0;
+// }
+
+to this one
+
+// if (fullscreen) {
+//     this._ios.providesPresentationContextTransitionStyle = true;
+//     this._ios.definesPresentationContext = true;
+//     this._ios.modalPresentationStyle = UIModalPresentationOverFullScreen;
+//     this._ios.view.backgroundColor = UIColor.clearColor;
+// }
+
+*/
