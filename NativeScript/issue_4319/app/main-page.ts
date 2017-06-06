d52e84824d8c1d68c5fd09aef144507ab5f859e6
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-06-06 10:50:24 +0300

    add

--- NativeScript/issue_4319/app/main-page.ts
+++ NativeScript/issue_4319/app/main-page.ts
@@ -13,14 +13,5 @@ export function navigatingTo(args: EventData) {
 
 export function onSearchLoaded(args) {
     sb = <SearchBar>args.object;
-    console.log("sb: " + sb);
-
-    console.log(sb.ios);
-
-    // sb.ios.barTintColor = UIColor.clearColor;
-
-    // sb.ios.barTintColor = new Color("#0e88a0").ios;
     sb.ios.searchBarStyle = UISearchBarStyle.UISearchBarStyleMinimal;
-    // sb.backgroundColor= new Color("#0e88a0");
-    // sb.borderColor = new Color("#0e88a0");
 }
