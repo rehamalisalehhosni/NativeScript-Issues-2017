965efb67883af017d7d099675bbdd341fe466fc9
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-06-06 08:16:01 +0300

    add

--- NativeScript/issue_4315/app/main-page.ts
+++ NativeScript/issue_4315/app/main-page.ts
@@ -10,28 +10,20 @@ export function onLoaded(args: EventData) {
     let page = <Page>args.object;
 
     let tv = <TextView>page.getViewById("tv");
-    console.log("tv: " + tv);
-    // tv.text = "Some really really really really really really really  long text.. I mean really really really really really really really really long (longer then 3 lines";
+    tv.text = "sample text";
 
     if (isAndroid) {
-        console.log("if android");
-
         let tvAndriod = tv.android;
-        console.log("tvAndroid: " + tvAndriod);
         tvAndriod.inputType = "text";
-        tvAndriod.maxLines = "1";
+        tvAndriod.maxLines = "3";
     } else if (isIOS) {
-        console.log("if ios");
-
         tv.ios.textContainer.maximumNumberOfLines = 3;
         tv.ios.textContainer.lineBreakMode = NSLineBreakByTruncatingTail;
     }
 
-    tv.text = "Some really really really really really really really reallyreally really really reallyreally really really reallyreally really really reallyreally really really  long text.. I mean really really really really really really really really  long text (LONGER THEN 3 Lines)";
-
     tv.on("textChange", () => {
         if (isAndroid) {
-            if (tv.android.getLayout().getLineCount() > 2) {
+            if (tv.android.getLayout().getLineCount() > 3) {
                 tv.android.getText().delete(tv.android.getText().length() - 1, tv.android.getText().length());
             }
         }
