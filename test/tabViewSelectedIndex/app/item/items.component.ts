2b5e326030ead650051cd3901c500bb9abc09dd3
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-06-14 17:39:36 +0300

    add

--- test/tabViewSelectedIndex/app/item/items.component.ts
+++ test/tabViewSelectedIndex/app/item/items.component.ts
@@ -9,17 +9,43 @@ import { SelectedIndexChangedEventData } from "ui/tab-view";
 })
 
 export class ItemsComponent {
-    public myTabItem: any;
-    public myTabItemSelected: any;
+    public myFirstItem: any;
+    public mySecondItem: any;
+    public myThirdItem: any;
 
-    constructor() { 
-        this.myTabItem  = { title: "NativeScript", iconSource: "~/images/icon.png" };
-        this.myTabItemSelected  = { title: "Selected", iconSource: "~/images/logo.png" };
+    constructor() {
+        this.myFirstItem = { title: "Selected", iconSource: "~/images/logo.png" };
+        this.mySecondItem = { title: "NativeScript", iconSource: "~/images/icon.png" };
+        this.myThirdItem = { title: "NativeScript", iconSource: "~/images/icon.png" };
     }
 
-    onIndexChange(args: SelectedIndexChangedEventData) {
-        console.log(args.newIndex);
-        console.log(args.oldIndex);
-        
+    onIndexChange(args) {
+        switch (args.value) {
+            case 0:
+                console.log(args.value);
+                this.myFirstItem = { title: "Selected", iconSource: "~/images/logo.png" };
+
+                this.mySecondItem = { title: "NativeScript", iconSource: "~/images/icon.png" };
+                this.myThirdItem = { title: "NativeScript", iconSource: "~/images/icon.png" };
+
+                break;
+            case 1: 
+                console.log(args.value);
+                this.myFirstItem = { title: "NativeScript", iconSource: "~/images/icon.png" };
+                this.mySecondItem = { title: "Selected", iconSource: "~/images/logo.png" };
+                this.myThirdItem = { title: "NativeScript", iconSource: "~/images/icon.png" };
+
+                console.log(this.mySecondItem.iconSource)
+                break;
+            case 2:
+                console.log(args.value);
+                this.myFirstItem = { title: "NativeScript", iconSource: "~/images/icon.png" };
+                this.mySecondItem = { title: "NativeScript", iconSource: "~/images/icon.png" };
+
+                this.myThirdItem = { title: "Selected", iconSource: "~/images/logo.png" };
+                break;
+            default:
+                break;
+        }
     }
 }
