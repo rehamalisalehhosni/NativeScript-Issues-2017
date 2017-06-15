17802453344acee0c6e30b8b9a7973bf9e2fd77e
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-06-15 14:43:03 +0300

    add

--- others/sample-ios-background-execution/app/custom-app-delegate.ts
+++ others/sample-ios-background-execution/app/custom-app-delegate.ts
@@ -11,15 +11,16 @@ export class CustomAppDelegate extends UIResponder implements UIApplicationDeleg
     private timerCounter;
 
     public applicationDidEnterBackground(application: UIApplication) {
+
         console.log("Enter background");
+
         this.bgTask = application.beginBackgroundTaskWithNameExpirationHandler("MyTask", () => {
             this.endBackgroundTask();
         });
 
         this.timerCounter = 5 * 60; // use this counter for 5 minutes X 60 seconds each
         console.log("Start logging numbers on background.");
-        this.timer = NSTimer.scheduledTimerWithTimeIntervalTargetSelectorUserInfoRepeats(1.00
-        , this, "runOnBackground", null, true); // NSTimer interval of 1 second
+        this.timer = NSTimer.scheduledTimerWithTimeIntervalTargetSelectorUserInfoRepeats(1.00, this, "runOnBackground", null, true); // NSTimer interval of 1 second
     }
 
     public applicationDidFinishLaunchingWithOptions(application: UIApplication, launchOptions: NSDictionary): boolean {
@@ -37,15 +38,19 @@ export class CustomAppDelegate extends UIResponder implements UIApplicationDeleg
         app.endBackgroundTask(this.bgTask);
 
         this.bgTask = UIBackgroundTaskInvalid;
-        console.log("End of background task.");
+        console.log("End of background task. current timerCount:");
     }
 
     public runOnBackground(): void {
+        
         if (this.timerCounter <= 0) {
             console.log("FIVE MINUTES PASSED - Execute the code here!");
             // Apple is not allowing any applicaiton to be bringed back to foreground without user interaction.
             // The code taht can be executed here is described by APple guidelines
             // You can also send notification from which the user can tap and bring the app to foreground manually.
+
+            // IMPORTANT: in the newer iOS versions beginBackgroundTaskWithNameExpirationHandler will give you only 3 minutes to run your task (compared to 10 minutes in older iOS versions)
+            // This is the longest amount of time that App;e are allowing for background task to execute. Using the timer you will notice taht you actually have about 174-175 seconds/
 
             this.endBackgroundTask(); // in this case the background task will be stopped and the timer will be reset via our timerCounter variable
             return;
