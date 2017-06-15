9d9ff1950f474ed080846a6bff1393f4eb996d62
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-06-15 14:06:18 +0300

    add

--- others/sample-ios-background-execution/app/custom-app-delegate.ts
+++ others/sample-ios-background-execution/app/custom-app-delegate.ts
@@ -1,9 +1,11 @@
+import { ios } from "utils/utils";
+
 export class CustomAppDelegate extends UIResponder implements UIApplicationDelegate {
     public static ObjCProtocols = [UIApplicationDelegate];
     public static ObjCExposedMethods = {
         "runOnBackground": { returns: interop.types.void }
     };
-    
+
     private bgTask;
     private timer;
     private timerCounter;
@@ -14,9 +16,10 @@ export class CustomAppDelegate extends UIResponder implements UIApplicationDeleg
             this.endBackgroundTask();
         });
 
-        this.timerCounter = 5;
+        this.timerCounter = 5 * 60; // use this counter for 5 minutes X 60 seconds each
         console.log("Start logging numbers on background.");
-        this.timer = NSTimer.scheduledTimerWithTimeIntervalTargetSelectorUserInfoRepeats(2, this, "runOnBackground", null, true);
+        this.timer = NSTimer.scheduledTimerWithTimeIntervalTargetSelectorUserInfoRepeats(1.00
+        , this, "runOnBackground", null, true); // NSTimer interval of 1 second
     }
 
     public applicationDidFinishLaunchingWithOptions(application: UIApplication, launchOptions: NSDictionary): boolean {
@@ -28,15 +31,20 @@ export class CustomAppDelegate extends UIResponder implements UIApplicationDeleg
             this.timer.invalidate();
             this.timer = null;
         }
-        this.timerCounter = 5;
-        UIApplication.sharedApplication().endBackgroundTask(this.bgTask);
+        this.timerCounter = 5 * 60; // reset the counter
+
+        var app = ios.getter(UIApplication, UIApplication.sharedApplication);
+        app.endBackgroundTask(this.bgTask);
+
         this.bgTask = UIBackgroundTaskInvalid;
         console.log("End of background task.");
     }
 
     public runOnBackground(): void {
         if (this.timerCounter <= 0) {
-            this.endBackgroundTask();
+            console.log("FIVE MINUTES PASSED - Execute the code here!");
+
+            this.endBackgroundTask(); // in this case the background task will be stopped and the timer will be reset via our timerCounter variable
             return;
         }
         console.log(`${this.timerCounter} (the app is on background)`);
