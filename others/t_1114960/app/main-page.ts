6505dbf4f1f3159e1b5894eab6bc6af61e925d57
Nick Iliev <nikolay.iliev@telerik.com>
2017-06-21 08:24:58 +0300

    add

--- others/t_1114960/app/main-page.ts
+++ others/t_1114960/app/main-page.ts
@@ -8,9 +8,17 @@ export function navigatingTo(args: EventData) {
     let page = <Page>args.object;
 
     let vm = new Observable();
-    let dataItems = [{ "id": 1, "name": "John" }, { "id": 2, "name": "Elizabeth" }, { "id": 3, "name": "Marry" }, { "id": 4, "name": "Chris" }, { "id": 2, "name": "Elizabeth" }, { "id": 3, "name": "Marry" }, 
-    { "id": 4, "name": "Chris" }, { "id": 2, "name": "Elizabeth" }, { "id": 3, "name": "Marry" }, { "id": 4, "name": "Chris" }, { "id": 2, "name": "Elizabeth" }, { "id": 3, "name": "Marry" }, { "id": 4, "name": "Chris" }, 
-    { "id": 2, "name": "Elizabeth" }, { "id": 3, "name": "Marry" }, { "id": 4, "name": "Chris" }, { "id": 2, "name": "Elizabeth" }, { "id": 3, "name": "Marry" }, { "id": 4, "name": "Chris" }, { "id": 2, "name": "Elizabeth" }, { "id": 3, "name": "Marry" }, { "id": 4, "name": "Chris" }];
+    let dataItems = [
+        { "id": 1, "name": "John" }, { "id": 2, "name": "Elizabeth" }, { "id": 3, "name": "Marry" },
+        { "id": 4, "name": "Chris" }, { "id": 5, "name": "Elizabeth" }, { "id": 6, "name": "Marry" },
+        { "id": 4, "name": "Chris" }, { "id": 8, "name": "Elizabeth" }, { "id": 9, "name": "Marry" },
+        { "id": 10, "name": "Chris" }, { "id": 11, "name": "Elizabeth" }, { "id": 12, "name": "Marry" },
+        { "id": 13, "name": "Chris" }, { "id": 14, "name": "Elizabeth" }, { "id": 15, "name": "Marry" },
+        { "id": 16, "name": "Chris" }, { "id": 17, "name": "Elizabeth" }, { "id": 18, "name": "Marry" },
+        { "id": 19, "name": "Chris" }, { "id": 20, "name": "Elizabeth" }, { "id": 21, "name": "Marry" },
+        { "id": 22, "name": "Elizabeth" }, { "id": 23, "name": "Marry" }, { "id": 24, "name": "Chris" },
+        { "id": 25, "name": "Elizabeth" }, { "id": 26, "name": "Marry" }, { "id": 27, "name": "Chris" },
+        { "id": 28, "name": "Elizabeth" }, { "id": 29, "name": "Marry" }, { "id": 30, "name": "Chris" }];
     vm.set("dataItems", dataItems);
 
     page.bindingContext = vm;
@@ -21,12 +29,10 @@ export function navigatingTo(args: EventData) {
     })
 }
 
-export function onTap() {
-    console.log(lv);
+export function scrollToIndex() {
+    lv.scrollToIndex(5);
+}
 
-    console.log(lv.android);
-
-    console.log(lv.scrollPosition);
-
-    lv.scrollToIndex(3);
+export function smoothScroll() {
+    lv._android.smoothScrollToPosition(11);
 }
