25c12f0347be166286bbd73ff4b0b29d9ee790b7
Nick Iliev <nikolay.iliev@telerik.com>
2017-06-22 17:14:33 +0300

    add

--- NativeScript/issue_4441/app/main-page.ts
+++ NativeScript/issue_4441/app/main-page.ts
@@ -8,38 +8,38 @@ import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
 import { HelloWorldModel } from './main-view-model';
 
-// Event handler for Page "navigatingTo" event attached in main-page.xml
-export function navigatingTo(args: EventData) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    let page = <Page>args.object;
-    
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and TypeScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
+import { WebView, LoadEventData } from "ui/web-view";
 
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
+let wv: WebView;
+
+export function navigatingTo(args: EventData) {
+
+    let page = <Page>args.object;
+wv = <WebView>page.getViewById("wv");
+
+wv.on("loadFinished", (args) => {
+    console.log("getActualSize")
+    console.log(wv.getActualSize().height);
+    console.log(wv.getActualSize().width);
+    console.log("getMeasuredHeight")
+    console.log(wv.getMeasuredHeight());
+    console.log("getMeasuredWidth")
+    console.log(wv.getMeasuredWidth());
+})
+
     page.bindingContext = new HelloWorldModel();
 }
 
 export function onWebViewLoaded(args) {
-    let wv = args.object;
+    var webview = args.object;
 
     setTimeout(function() {
         console.log("getActualSize")
-        console.log(wv.getActualSize().height);
-        console.log(wv.getActualSize().width);
+        console.log(webview.getActualSize().height);
+        console.log(webview.getActualSize().width);
         console.log("getMeasuredHeight")
-        console.log(wv.getMeasuredHeight());
+        console.log(webview.getMeasuredHeight());
         console.log("getMeasuredWidth")
-        console.log(wv.getMeasuredWidth());
+        console.log(webview.getMeasuredWidth());
     }, 100);
 }
