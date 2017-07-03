d329a0c8efa4a3d301e78c0f3ebc990215b829f9
Nick Iliev <nikolay.iliev@telerik.com>
2017-07-03 17:19:06 +0300

    add

--- NativeScript/issue_4488/app/item/items.component.ts
+++ NativeScript/issue_4488/app/item/items.component.ts
@@ -6,8 +6,7 @@ import { ItemService } from "./item.service";
 @Component({
     selector: "ns-items",
     moduleId: module.id,
-    templateUrl: "./items.component.html",
-    styleUrls: ["./items.component.css"]
+    templateUrl: "./items.component.html"
 })
 export class ItemsComponent implements OnInit {
     items: Item[];
