fcb0dac892e54c6081f577a7e2e8d0850d68455f
Nick Iliev <nikolay.iliev@telerik.com>
2017-07-14 15:20:18 +0300

    add

---
+++ others/t_1119409/app/item/items.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Item } from "./item";
+import { ItemService } from "./item.service";
+
+@Component({
+    selector: "ns-items",
+    moduleId: module.id,
+    templateUrl: "./items.component.html",
+})
+
+export class ItemsComponent implements OnInit {
+    public chartData: Array<any>;
+    public targetData: number = 12;
+    public categoryAxisField: string = "MeasurementDate";
+    public valueAxisField: string = "DistanceKM";
+    public targetValueAxisField: string = "Duration";
+
+    public dateFormat: string = "MM/dd/YYYY";
+    ngOnInit(): void {
+        this.chartData = [
+            { MeasurementDate: "2017-07-10T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 5.83, DistanceMiles: 1.6 },
+            { MeasurementDate: "2017-07-10T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 5.83, DistanceMiles: 1.6 },
+            { MeasurementDate: "2017-07-10T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 5.83, DistanceMiles: 1.6 },
+            { MeasurementDate: "2017-07-11T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 6.83, DistanceMiles: 2.6 },
+            { MeasurementDate: "2017-07-12T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 7.83, DistanceMiles: 3.6 },
+            { MeasurementDate: "2017-07-13T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 5.83, DistanceMiles: 1.6 },
+            { MeasurementDate: "2017-07-14T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 8.83, DistanceMiles: 3.6 },
+            { MeasurementDate: "2017-07-15T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 7.83, DistanceMiles: 2.6 },
+            { MeasurementDate: "2017-07-16T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 2.83, DistanceMiles: 5.6 },
+            { MeasurementDate: "2017-07-17T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 1.83, DistanceMiles: 9.6 },
+
+        ];
+    }
+}
