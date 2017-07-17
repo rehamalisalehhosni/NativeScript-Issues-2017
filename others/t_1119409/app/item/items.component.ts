f3b6678b22a5ead8253868b0d99635cee670ce57
Nick Iliev <nikolay.iliev@telerik.com>
2017-07-17 09:53:08 +0300

    add

--- others/t_1119409/app/item/items.component.ts
+++ others/t_1119409/app/item/items.component.ts
@@ -1,22 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 
-import { Item } from "./item";
-import { ItemService } from "./item.service";
-
 @Component({
-    selector: "ns-items",
     moduleId: module.id,
-    templateUrl: "./items.component.html",
+    templateUrl: "items.component.html"
 })
 
 export class ItemsComponent implements OnInit {
+    
     public chartData: Array<any>;
     public targetData: number = 12;
     public categoryAxisField: string = "MeasurementDate";
     public valueAxisField: string = "DistanceKM";
     public targetValueAxisField: string = "Duration";
-
     public dateFormat: string = "MM/dd/YYYY";
+
     ngOnInit(): void {
         this.chartData = [
             { MeasurementDate: "2017-07-10T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 5.83, DistanceMiles: 1.6 },
@@ -29,7 +26,6 @@ export class ItemsComponent implements OnInit {
             { MeasurementDate: "2017-07-15T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 7.83, DistanceMiles: 2.6 },
             { MeasurementDate: "2017-07-16T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 2.83, DistanceMiles: 5.6 },
             { MeasurementDate: "2017-07-17T00:00:00", MeasurementTime: "2001-01-01T02:00:00", Duration: 7, DistanceKM: 1.83, DistanceMiles: 9.6 },
-
         ];
     }
 }
