e3c4fe13e0c94c12e560331daf6e55b18a275989
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-08-08 09:47:56 +0300

    add

--- others/t_1123779/app/main-page.ts
+++ others/t_1123779/app/main-page.ts
@@ -1,26 +1,22 @@
 import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
-var NACamera = require("nativescript-na-camera");
+import { Image } from "ui/image";
+import { ImageSource } from "image-source";
 
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
     let page = <Page>args.object;
-    NACamera.start();
-}
-export function capturePhoto(args: EventData) {
-    NACamera.capturePhoto({
-        saveToLibrary: true
-    }).then(function (image, savedToLibrary) {
-        NACamera.stop();
-        if (savedToLibrary) {
-            console.log("Photo was saved to library!");
-        }
-        // Do something more... 
-    }, function (error) {
-        console.error(error);
-    });
 }
 
-export function newPhoto(args: EventData) {
-    NACamera.start();
+export function onLoaded(args: EventData) {
+    let img = <Image>args.object;
+    let imgSource = <ImageSource>img.imageSource;
+
+    console.log(imgSource.ios); // UIImage
+
+    // convert theUIImage to data buffer again uisng the marshalling techniques
+    // e.g., here is an Objective-C examples 
+    // https://stackoverflow.com/questions/448125/how-to-get-pixel-data-from-a-uiimage-cocoa-touch-or-cgimage-core-graphics
+
+    let jpeg = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imgSource.ios);
 }
