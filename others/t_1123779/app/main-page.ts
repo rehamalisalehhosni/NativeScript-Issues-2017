3ce0f76b4ff50bb2c630a96e8df1321ec7e5b99a
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-08-11 08:36:20 +0300

    add

--- others/t_1123779/app/main-page.ts
+++ others/t_1123779/app/main-page.ts
@@ -3,20 +3,15 @@ import { Page } from 'ui/page';
 import { Image } from "ui/image";
 import { ImageSource } from "image-source";
 
+import { getCurrentLocation } from "nativescript-geolocation";
+
+var camera = require("nativescript-na-camera");
+
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
     let page = <Page>args.object;
 }
 
-export function onLoaded(args: EventData) {
-    let img = <Image>args.object;
-    let imgSource = <ImageSource>img.imageSource;
-
-    console.log(imgSource.ios); // UIImage
-
-    // convert theUIImage to data buffer again uisng the marshalling techniques
-    // e.g., here is an Objective-C examples 
-    // https://stackoverflow.com/questions/448125/how-to-get-pixel-data-from-a-uiimage-cocoa-touch-or-cgimage-core-graphics
-
-    let jpeg = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imgSource.ios);
+export function capturePhoto() {
+    camera.capturePhoto();
 }
